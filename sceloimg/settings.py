b78110316cc3816d0013737520eeef4123584d5c
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2024-11-19 16:58:37 +0600

    Hello world to vercel

--- sceloimg/settings.py
+++ sceloimg/settings.py
@@ -112,6 +112,11 @@ USE_I18N = True
 USE_TZ = True
 
 
+# api/settings.py
+WSGI_APPLICATION = 'api.wsgi.app'
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
