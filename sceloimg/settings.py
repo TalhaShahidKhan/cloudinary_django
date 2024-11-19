1f59282ea6b01c662a7acd8ec488638622158e59
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2024-11-19 16:56:13 +0600

    Hello world to vercel

--- sceloimg/settings.py
+++ sceloimg/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*tq&-osqrwk%j3o3uq#7^ht@r=tdu11rne59z)=hr8wj&7yaap
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','.vercel.app','127.0.0.1']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sceloimg.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
