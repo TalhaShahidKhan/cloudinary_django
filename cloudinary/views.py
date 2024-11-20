195cd57be227c8f9693cf40798c33c0a9acef08f
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2024-11-21 00:41:13 +0600

    deploy error check

--- cloudinary/views.py
+++ cloudinary/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,"cloud/home.html")
+    return HttpResponse("hello world")
